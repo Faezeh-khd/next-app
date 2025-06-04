6463e8fc3e613434bfedc552f5cf282849fb2394
Faezeh-khd <fa.khadem18@gmail.com>
2025-06-04 18:00:24 +0330

    data fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
-const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+import React from "react";
+interface user {
+  id: number;
+  name: string;
 }
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: user[] = await res.json();
 
-export default UsersPage
+  return (
+    <>
+      <h1>Users</h1>
+
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default UsersPage;
