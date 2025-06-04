5db99849b3caf3cb6eb52d143716cd952830fd2f
Faezeh-khd <fa.khadem18@gmail.com>
2025-06-04 17:16:10 +0330

    navigation and routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 //import Image from "next/image";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-  <main>
-    <h1>hello world</h1>
-  </main>
+    <main>
+      <h1>hello world</h1>
+      <Link href="/users">users</Link>
+      {/* Using Next.js's Link component improves navigation efficiency by preventing the redownload of shared resources like CSS and JavaScript. This enhances the loading speed of web applications. */}
+    </main>
   );
+
 }
